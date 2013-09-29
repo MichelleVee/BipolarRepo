d0661347a4a73019e8e8ce826d505ee21b7b6d0c
Kappy0 <ryanklaproth@gmail.com>
2013-09-29 00:01:26 -0500

    Timer and Depth Meter
    Added Depth Meter and Timer functionality.

--- Project Bluelight/TestProtoDepressionLevel/Project Bluelight PROTO/script.js
+++ Project Bluelight/TestProtoDepressionLevel/Project Bluelight PROTO/script.js
@@ -26,6 +26,8 @@ var leftHeld, upHeld, rightHeld;
 
 //Initialize game variables
 var player, enemy, glass;
+var timerMsg;
+var depthMeter, randDepth;
 var gravity = 0.3;
 
 //Key event initialization
@@ -56,9 +58,54 @@ function init()
     player.y = canvas.height / 2;
     stage.addChild(player);
 
+    //Create the timer
+    timerMsg = new createjs.Text('30', 'Bold 25px Arial', 'black');
+    timerMsg.x = 20;
+    timerMsg.y = 20;
+    stage.addChild(timerMsg);
+
+    //This is for the event listener added to the Ticker for the timer.
+    //Thus, it is put in the init() function as part of initializing the timer.
+    var delay = 60;
+    var timer = function()
+    {
+        if(delay <= 0)
+        {
+            timerMsg.text = parseInt(timerMsg.text - 1);
+            delay = 60;
+        }
+        delay--;
+    }
+
+    //Initialize the Depth Meter
+    depthMeter = new createjs.Shape();
+    depthMeter.graphics.beginFill("black").drawRect(650, 20, 25, 150);
+    stage.addChild(depthMeter);
+
+    var randDelay = 60;
+    randDepth = 0;
+
+    randTimerMsg = new createjs.Text(randDepth, 'Bold 25px Arial', 'black');
+    randTimerMsg.x = 50;
+    randTimerMsg.y = 20;
+    stage.addChild(randTimerMsg);
+
+    var randTimer = function()
+    {
+        if(randDelay <= 0)
+        {
+            randDepth = Math.floor(Math.random() * 5);
+            randTimerMsg.text = randDepth;
+            randDelay = Math.floor(Math.random() * 601);
+        }
+        randDelay--;
+    }
+
     //Set the update loop
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addListener(window);
+    createjs.Ticker.addEventListener('tick', timer);
+    createjs.Ticker.addEventListener('tick', randTimer);
 }
 
 //Game Loop
@@ -84,6 +131,26 @@ function tick()
 	gravitate(player);
 	
     player.onClick = knockBack;
+
+    //For the depth meter simulation
+    switch(randDepth)
+    {
+        case 0:
+            depthMeter.graphics.clear().beginFill("black").drawRect(650, 20, 25, 150);
+            break;
+        case 1:
+            depthMeter.graphics.clear().beginFill("black").drawRect(650, 20, 25, 125);
+            break;
+        case 2:
+            depthMeter.graphics.clear().beginFill("black").drawRect(650, 20, 25, 100);
+            break;
+        case 3:
+            depthMeter.graphics.clear().beginFill("black").drawRect(650, 20, 25, 75);
+            break;
+        case 4:
+            depthMeter.graphics.clear().beginFill("black").drawRect(650, 20, 25, 50);
+            break;
+    }
 
     stage.update();
 }
